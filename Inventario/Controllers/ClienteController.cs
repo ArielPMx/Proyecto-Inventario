9e77d477f5153616fc40d38b4726012d736d5202
Ariel P <arielpoueriet@gmail.com>
2022-11-07 10:31:48 -0400

    Segundo modulo

---
+++ Inventario/Controllers/ClienteController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Negocio;
+using Datos;
+
+namespace Inventario.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        ClienteModelNegocio clienteNegocio = new ClienteModelNegocio();
+
+        public ActionResult Index()
+        {
+            return View(clienteNegocio.ObtenerClientes());
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View(clienteNegocio.HacerCliente());
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(ClienteModel model)
+        {
+            clienteNegocio.AgregarCliente(model);
+            return RedirectToAction("Index", "Cliente");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            return View(clienteNegocio.ObtenerCliente(id));
+        }
+
+        [HttpPost]
+        public ActionResult Editar(ClienteModel model)
+        {
+            clienteNegocio.EditarCliente(model);
+            return RedirectToAction("Index", "Cliente");
+        }
+
+        public ActionResult Borrar(int id)
+        {
+            return View(clienteNegocio.ObtenerCliente(id));
+        }
+
+        [HttpPost]
+        public ActionResult Borrar(ClienteModel model)
+        {
+            clienteNegocio.BorrarCliente(model);
+            return RedirectToAction("Index", "Cliente");
+        }
+    }
+}
---
+++ Negocio/ClienteModelNegocio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+
+namespace Negocio
+{
+    public class ClienteModelNegocio
+    {
+        ClienteModel clienteModel = new ClienteModel();
+        public List<ClienteModel> ObtenerClientes()
+        {
+            var model = clienteModel.ObtenerClientes();
+            return model;
+        }
+        public ClienteModel ObtenerCliente(int id)
+        {
+            var model = clienteModel.ObtenerCliente(id);
+            return model;
+        }
+        public void AgregarCliente(ClienteModel model)
+        {
+            model.Agregar(model);
+        }
+        public void EditarCliente(ClienteModel model)
+        {
+            model.Editar(model);
+        }
+        public void BorrarCliente(ClienteModel model)
+        {
+            model.Borrar(model);
+        }
+        public ClienteModel HacerCliente()
+        {
+            return clienteModel.HacerNuevoCliente();
+        }
+    }
+}
