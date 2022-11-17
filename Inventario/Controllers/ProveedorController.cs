245f1f72c3f4f5b84f6fdf5b4c648425fa507ec2
Ariel P <arielpoueriet@gmail.com>
2022-11-17 15:25:40 -0400

    Tercer Modulo Fixed

---
+++ Inventario/Controllers/ProveedorController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Negocio;
+using Datos;
+
+namespace Inventario.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        // GET: Proveedor
+        ProveedorModelNegocio proveedorNegocio = new ProveedorModelNegocio();
+
+        public ActionResult Index()
+        {
+            return View(proveedorNegocio.ObtenerProveedores());
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(ProveedorModel model)
+        {
+            proveedorNegocio.AgregarProveedor(model);
+            return RedirectToAction("Index", "Proveedor");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            return View(proveedorNegocio.ObtenerProveedor(id));
+        }
+
+        [HttpPost]
+        public ActionResult Editar(ProveedorModel model)
+        {
+            proveedorNegocio.EditarProveedor(model);
+            return RedirectToAction("Index", "Proveedor");
+        }
+
+        public ActionResult Borrar(int id)
+        {
+            return View(proveedorNegocio.ObtenerProveedor(id));
+        }
+
+        [HttpPost]
+        public ActionResult Borrar(ProveedorModel model)
+        {
+            proveedorNegocio.BorrarProveedor(model);
+            return RedirectToAction("Index", "Proveedor");
+        }
+    }
+}
---
+++ Negocio/ProveedorModelNegocio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+
+namespace Negocio
+{
+    public class ProveedorModelNegocio
+    {
+        ProveedorModel proveedorModel = new ProveedorModel();
+        public List<ProveedorModel> ObtenerProveedores()
+        {
+            var model = proveedorModel.ObtenerProveedores();
+            return model;
+        }
+
+        public ProveedorModel ObtenerProveedor(int id)
+        {
+            var model = proveedorModel.ObtenerProveedor(id);
+            return model;
+        }
+
+        public void AgregarProveedor(ProveedorModel model)
+        {
+            model.Agregar(model);
+        }
+        public void EditarProveedor(ProveedorModel model)
+        {
+            model.Editar(model);
+        }
+        public void BorrarProveedor(ProveedorModel model)
+        {
+            model.Borrar(model);
+        }
+    }
+}
