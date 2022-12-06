83ae1569f7cfbe8dbd2ba5df3fa8ba68a9608c11
Ariel P <arielpoueriet@gmail.com>
2022-12-06 16:53:12 -0400

    MOudlo stock

---
+++ Inventario/Controllers/StockController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Negocio;
+using Datos;
+
+namespace Inventario.Controllers
+{
+    public class StockController : Controller
+    {
+        StockModelNegocio stockModel = new StockModelNegocio();
+
+        // GET: Stock
+        public ActionResult Index()
+        {
+            return View(stockModel.ObtenerStocks());
+        }
+
+        public ActionResult VerMovimientos(int id)
+        {
+            return View(stockModel.ObtenerStock(id));
+        }
+    }
+}
